43f2dde30eb4b220ac24747862e7a351155b7f05
Pruthvi <146100300+pruthvishetty1789@users.noreply.github.com>
2025-08-01 19:42:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,3 +9,6 @@ app.use("/api", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
